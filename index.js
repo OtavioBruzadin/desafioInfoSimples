68518c4e8571194cc903cd6de6cca4d56ae87bbe
OtavioBruzadin <otavio.bruzadin2004@gmail.com>
2024-03-05 02:03:44 -0300

    Refactor: categories are properly separated

--- index.js
+++ index.js
@@ -9,20 +9,37 @@ const PORT = 8080;
 const server = express();
 
 const URL = "https://infosimples.com/vagas/desafio/commercia/product.html"
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+server.listen(PORT);
 
  axios(URL).then(response => {
      const html = response.data
      const parsedHtml = cheerio.load(html)
-     let title= parsedHtml('h2#product_title').text()
-     let brand= parsedHtml('div.brand').text()
-     let description = parsedHtml('div.proddet p').text()
-     const categories=[]
-     console.log(title)
-     console.log(brand)
-     console.log(description)
-     categories.push(parsedHtml('nav a').append(" ").text())
-     console.log( categories)
+     const  respostaFinal = {}
+     function parseCategories() {
+         const categories = []
+         let category = ""
 
- }).catch(err => console.log(err))
+         parsedHtml('nav.current-category ').find('a').each((index, piece) => {
+             category = parsedHtml(piece).text()
+             categories.push(category)
+
+         })
+         return categories
+     }
+     respostaFinal['title'] =  parsedHtml('h2#product_title').text()
+     respostaFinal['brand'] = parsedHtml('div.brand').text()
+     respostaFinal['description'] = parsedHtml('div.proddet p').text()
+     respostaFinal['categories']= parseCategories()
+
+     const jsonRespostaFinal = JSON.stringify(respostaFinal);
+
+     fs.writeFile('produto.json', jsonRespostaFinal, function (err) {
+         if (err) {
+
+             console.log(err);
+         } else {
+             console.log('Arquivo salvo');
+         }
+     })
+ }).catch()
 
